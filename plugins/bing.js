49c7f1537da29286a70aeda833489be9deac74f4
Aromal <136228489+Loki-Xer@users.noreply.github.com>
2024-02-10 13:10:45 +0530

    fixed!!!

--- plugins/bing.js
+++ plugins/bing.js
@@ -1,5 +1,6 @@
 const {
 	inrl,
+	mode,
 	config,
 	getBuffer,
 } = require('../lib');
@@ -8,6 +9,7 @@ const {
 inrl({
 	pattern: 'bing ?(.*)',
 	type: "eva",
+	fromMe: mode,
 	desc: "bing ai",
 }, async (message, match) => {
 	match = match || message.reply_message.text;
