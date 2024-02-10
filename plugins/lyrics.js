6685d985f1f1aa0913a8eafc0a33e8c899b032bb
Aromal <136228489+Loki-Xer@users.noreply.github.com>
2024-02-10 19:27:43 +0530

    lyrics fixed

--- plugins/lyrics.js
+++ plugins/lyrics.js
@@ -13,12 +13,12 @@ inrl({
 	fromMe: mode,
 	desc: lang.LYRICS.DESC,
 	type: "search"
-}, async (m, match) => {
-	match = match || m.reply_message.text;
-	if (!match) return await m.send(lang.BASE.TEXT);
+}, async (message, match) => {
+	match = match || message.reply_message.text;
+	if (!match) return await message.send(lang.BASE.TEXT);
 	const res = await fetchJson(`${config.BASE_URL}api/search/lyrics?text=${match}&apikey=${config.INRL_KEY}`);
 	if (!res.status) return await message.send(`Please enter a new apikey, as the given apikey limit has been exceeded. Visit ${config.BASE_URL}api/signup for gettig a new apikey. setvar inrl_key: your apikey`)
-	if (!res.result) return m.send(lang.BASE.ERROR.format(",try again"));
+	if (!res.result) return message.send(lang.BASE.ERROR.format(",try again"));
 	const {
 		thumb,
 		lyrics,
@@ -26,12 +26,12 @@ inrl({
 		artist
 	} = res.result, tbl = "```", tcl = "*_", tdl = "_*";
 	const msg = lang.LYRICS.RESPONCE.format(tcl + artist + tdl, tcl + title + tdl) + `\n\n${tbl}${lyrics}${tbl}`;
-	return await m.client.sendMessage(m.from, {
+	return await message.client.sendMessage(message.from, {
 		image: {
 			url: thumb
 		},
 		caption: msg
 	}, {
-		quoted: m
+		quoted: message
 	})
 });
